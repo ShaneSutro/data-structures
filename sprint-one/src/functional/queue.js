5b74c4b6fd2167a3926b69a2f1eb034a82a12fce
Shane Sutro <shanesutro@gmail.com>
2020-10-19 22:01:37 -0600

    Begin work on enqueue for functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -6,14 +6,39 @@ var Queue = function() {
 
   // Implement the methods below
 
+  //I-OCE:
+  //Input: a primitive value type
+  //Output: a primitive value type
+  //Constraints:
+  //For enquque: should add a value to the front of the object
+  //For deququq: should return a value from the end of the object
+  //Edge cases
+  //None?
+  var numberOfItems = 0;
   someInstance.enqueue = function(value) {
-    var queue = [];
+
+    for (var key in storage) {
+      if (Number(key + 1) > numberOfItems) {
+        numberOfItems = Number(key + 1);
+      }
+    }
+    if (numberOfItems > 0) {
+      for (var i = numberOfItems - 1; i >= 0; i--) {
+        var newKey = i + 1;
+        storage['' + newKey] = storage[i.toString()];
+      }
+    }
+
+    storage['0'] = value;
+
+    return storage;
   };
 
   someInstance.dequeue = function() {
   };
 
   someInstance.size = function() {
+    return numberOfItems + 1;
   };
 
   return someInstance;
