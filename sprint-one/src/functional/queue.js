6320b0dc99d3500e965ab37be235936c1e39ff51
Shane Sutro <shanesutro@gmail.com>
2020-10-20 17:08:04 -0600

    Complete functional stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -6,50 +6,28 @@ var Queue = function() {
 
   // Implement the methods below
 
-  //For enqueue:
-  //Get the number of items
-  //Shift everything up one
-  //Add value at index 0
-  //update numberOfItems counter
-
-  //For dequeue:
-  //Using numberOfItems, set index to numberOfItems - 1
-  //Use index to remove the last value from the array, delete it, then return it
-
   var numberOfItems = 0;
   var index = -1;
 
   someInstance.enqueue = function(value) {
-    index = 0;
-    for (var key in storage) {
-      if (Number(key) > index) {
-        numberOfItems = Number(key + 1);
-        index = Number(key);
-      }
-    }
-    if (numberOfItems > 0) {
-      for (var i = numberOfItems - 1; i >= 0; i--) {
-        var newKey = i + 1;
-        storage['' + newKey] = storage[i.toString()];
-      }
+    var numberOfItems = _.size(storage);
+    for (var i = numberOfItems - 1; i >= 0; i--) {
+      var newKey = i + 1;
+      storage['' + newKey] = storage[i.toString()];
     }
 
     storage['0'] = value;
-    numberOfItems++;
   };
 
   someInstance.dequeue = function() {
-    var indexToReturn = (numberOfItems - 1).toString();
-    var returnValue = storage[indexToReturn];
-    delete storage[indexToReturn];
-    numberOfItems--;
-    numberOfItems < 0 ? numberOfItems = 0 : numberOfItems;
-    console.log(numberOfItems, index, returnValue);
+    var indexToRemove = (_.size(storage) - 1).toString();
+    var returnValue = storage[indexToRemove];
+    delete storage[indexToRemove];
     return returnValue;
   };
 
   someInstance.size = function() {
-    return numberOfItems;
+    return _.size(storage);
   };
 
   return someInstance;
